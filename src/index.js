69c1fb3040b30c93946f554a2f0aee6baffa84c4
burakndmr <buraknasifdemir123@gmail.com>
2022-04-23 17:56:42 +0300

    some issues

--- src/index.js
+++ src/index.js
@@ -1,18 +1,16 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+
 import * as ReactDOMClient from "react-dom/client";
 import "./Style/style.css";
 import App from "./App";
 import { ContextProvider } from "./Context/MainContext";
 const container = document.getElementById("root");
 const root = ReactDOMClient.createRoot(container);
-<Route path="/" component={App} />;
+
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <ContextProvider>
-        <App />
-      </ContextProvider>
-    </BrowserRouter>
+    <ContextProvider>
+      <App />
+    </ContextProvider>
   </React.StrictMode>
 );
