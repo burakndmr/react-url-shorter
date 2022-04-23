e29ff4e98f6cf29425554af253998e0c7f9162ba
burakndmr <buraknasifdemir123@gmail.com>
2022-04-23 14:51:10 +0300

    ...

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import React from "react";
-
+import { BrowserRouter, Route } from "react-router-dom";
 import * as ReactDOMClient from "react-dom/client";
 import "./Style/style.css";
 import App from "./App";
 import { ContextProvider } from "./Context/MainContext";
 const container = document.getElementById("root");
 const root = ReactDOMClient.createRoot(container);
-
+<Route path="/" component={App} />;
 root.render(
   <React.StrictMode>
-    <ContextProvider>
-      <App />
-    </ContextProvider>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <ContextProvider>
+        <App />
+      </ContextProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
